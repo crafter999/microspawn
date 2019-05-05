7b2b35fc5eeee11b3ee173daa75e73c53ffb2d23
crafter999 <drakos.git@gmail.com>
2019-05-05 20:03:36 +0300

    Support streaming a process | Outsource stringToArray args

--- main.js
+++ main.js
@@ -9,18 +9,7 @@ class microspawn {
    run(program, args = []) {
       return new Promise((resProm, rejProm) => {
          // convert string args to array
-         if (typeof args === "string") {
-            // support escaping
-            if (args.includes('"') || args.includes("'")) {
-               args = this._escapeArgs(args);
-            } else {
-               args = args.split(" ");
-            }
-
-         }
-
-         // trim empty args
-         args = args.filter(e => e);
+         args = this._stringToArray(args);
 
          // run command
          let child = spawn(program, args, this._options);
@@ -55,15 +44,50 @@ class microspawn {
 
    // execute & log out the result
    async log(program, args) {
-      let result = await this.run(program, args).catch(e => {
-         console.error(e);
-         process.exit(1);
-      });
+      let result = await this.run(program, args).catch(e => this._exit(e));
       console.log(result);
    }
 
    async script(scriptContents) {
       return this.run("/bin/sh", ["-c", scriptContents]);
+   }
+
+   // read only stream
+   stream(program, args = []) {
+      // convert string args to array
+      args = this._stringToArray(args);
+
+      // run command
+      let child = spawn(program, args, this._options);
+
+      let chunkError = "";
+      child.stderr.on("data", (e) => {
+         chunkError += e;
+      });
+      child.on("error", (e) => {
+         this._exit(e)
+      });
+      child.on("close", (/* optional_code */) => {
+         chunkError === "" ? process.exit(0) : this._exit(chunkError);
+      });
+
+      return child.stdout;
+   }
+
+   _stringToArray(args) {
+      if (typeof args === "string") {
+         // support escaping
+         if (args.includes('"') || args.includes("'")) {
+            args = this._escapeArgs(args);
+         } else {
+            args = args.split(" ");
+         }
+      }
+
+      // trim empty args
+      args = args.filter(e => e);
+
+      return args;
    }
 
    _escapeArgs(args) {
@@ -73,16 +97,27 @@ class microspawn {
 
       for (let q of quotesHack) {
          for (let m = 0; m <= escaped.length; m++) {
-            if (q === "quotes" && escaped[m]!=="") {
+            if (q === "quotes" && escaped[m] !== "") {
                q = escaped[m];
                escaped[m] = ""; // *remove* it from queue
             }
          }
+         // strange windows bug: replace single quotes with double
+         if (process.platform === "win32")
+            q = q.replace(/'/g, '"');
+
          result.push(q);
       }
 
       return result;
    }
+
+   _exit(e) {
+      console.error(e);
+      process.exit(1);
+   }
+
+
 }
 
 module.exports = microspawn;
